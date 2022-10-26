9d1108ee515dae1b199ae717bb600da91e958ef3
Atsushi Toyama <atsushi.tohyama.160.333@gmail.com>
2022-10-26 21:53:18 +0900

    Add stack

---
+++ sources/forth_modoki/04_stack/stack.h
@@ -0,0 +1,7 @@
+#include "token.h"
+
+void stack_push(struct Token *t);
+
+struct Token *stack_pop(void);
+
+void stack_print_all(void);
---
+++ sources/forth_modoki/04_stack/token.h
@@ -0,0 +1,25 @@
+
+enum LexicalType {
+    NUMBER,
+    SPACE,
+    EXECUTABLE_NAME,
+    LITERAL_NAME,
+    OPEN_CURLY,
+    CLOSE_CURLY,
+    END_OF_FILE,
+    UNKNOWN
+};
+
+struct Token {
+    enum LexicalType ltype;
+    union {
+        int number;
+        char onechar;
+        char *name;
+    } u;
+};
+
+void token_print(struct Token *t);
+
+struct Token *new_token(enum LexicalType ty);
+struct Token *new_num_token(int val);
