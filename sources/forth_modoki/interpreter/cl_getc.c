090b34d3122aef91a8e74e80700c022873d5c7e4
Atsushi Toyama <atsushi.tohyama.160.333@gmail.com>
2022-10-29 18:15:47 +0900

    Refactor APIs in cl_getc.c
    - Make complexity of 'cl_getc' to O(1)
    - Write void explicitly in the signature of 'cl_getc'

--- sources/forth_modoki/interpreter/cl_getc.c
+++ sources/forth_modoki/interpreter/cl_getc.c
@@ -1,17 +1,16 @@
 #include "clesson.h"
 #include <string.h>
 
-static const char* input = "123 456";
+static const char *input = "123 456";
 static int pos = 0;
 
-
-int cl_getc() {
-    if(strlen(input) == pos)
+int cl_getc(void) {
+    if (input[pos] == '\0')
         return EOF;
     return input[pos++];
 }
 
-void cl_getc_set_src(char* str){
+void cl_getc_set_src(const char *str) {
     input = str;
     pos = 0;
 }
