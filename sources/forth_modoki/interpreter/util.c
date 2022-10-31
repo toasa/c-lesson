834153b2c6158968acf0ab2706a634f3a24537aa
Atsushi Toyama <atsushi.tohyama.160.333@gmail.com>
2022-10-31 20:34:54 +0900

    Add expect() to specify the expected condition and error message

--- sources/forth_modoki/interpreter/util.c
+++ sources/forth_modoki/interpreter/util.c
@@ -1,3 +1,5 @@
+#include <stdarg.h>
+
 #include "clesson.h"
 
 bool streq(const char *s1, const char *s2) { return strcmp(s1, s2) == 0; }
@@ -13,5 +15,15 @@ void assert_str_eq(const char *s1, const char *s2) {
     if (!streq(s1, s2)) {
         fprintf(stderr, "%s != %s\n", s1, s2);
         abort();
+    }
+}
+
+void expect(bool b, const char *fmt, ...) {
+    if (!b) {
+        va_list ap;
+        va_start(ap, fmt);
+        vfprintf(stderr, fmt, ap);
+        fprintf(stderr, "\n");
+        exit(1);
     }
 }
