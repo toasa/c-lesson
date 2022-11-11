b461e9b66ca67dd9a35afc8543dda252dc01e8b1
Atsushi Toyama <atsushi.tohyama.160.333@gmail.com>
2022-11-11 23:34:38 +0900

    Solve print_hex.s

--- sources/arm_asm/02_arm_assembly/2_3_print_hex/print_hex.s
+++ sources/arm_asm/02_arm_assembly/2_3_print_hex/print_hex.s
@@ -7,8 +7,48 @@
 .globl _start
 _start:
     ldr r1,=0xdeadbeaf
-    b print_hex
+    ldr r2,=0x101f1000
+    ldr r3,=hexchar
+
+    // Calculate number of digits
+    mov r8, r1
+    mov r9, #0
+calc_num_of_digit:
+    add r9, r9, #1
+    lsr r8, r8, #4
+    cmp r8, #0
+    bne calc_num_of_digit
+
+    // Print '0x'
+    mov r4, #0x30
+    str r4, [r2]
+    mov r4, #0x78
+    str r4, [r2]
+
 print_hex:
-    // TODO: implement here
+    // Get half byte. From most significant to least.
+    mov r4, r9
+    sub r4, #1
+    lsl r4, #2
+    lsr r4, r1, r4
+    and r4, r4, #0xF
+
+    // Print hex
+    add r4, r4, r3
+    ldrb r5, [r4]
+    str r5, [r2]
+
+    // Shift and jump if needed
+    sub r9, #1 // decrement counter
+    cmp r9, #0
+    bne print_hex
+
+    // Print newline
+    mov r4, #0x0A
+    str r4, [r2]
+
 end:
     b end
+
+hexchar:
+    .asciz "0123456789ABCDEF"
