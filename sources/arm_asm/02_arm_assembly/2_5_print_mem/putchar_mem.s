f54a7a68ad7b93c9738ad860bcb0026f42bd90bf
Atsushi Toyama <atsushi.tohyama.160.333@gmail.com>
2022-11-12 20:55:33 +0900

    Solve putchar_mem.s

--- sources/arm_asm/02_arm_assembly/2_5_print_mem/putchar_mem.s
+++ sources/arm_asm/02_arm_assembly/2_5_print_mem/putchar_mem.s
@@ -1,31 +1,42 @@
 .globl _start
 _start:
-
+  ldr r13,=0x08000000 // Set stack pointer
   ldr r0,=msg1
   bl print
   ldr r0,=msg2
   bl print
 end:
-  b end
+  b end 
 
+//
+// arg r3: one character is printed.
+// used internal register: r1
+//
 putchar:
-  // TODO: fix here!
   ldr r1,=0x101f1000
-  ldr r0, [r1]
-  mov r15, r14
+  str r3, [r1]
+  mov r15, r14 
 
-
+//
+// arg r0: address of the string which is printed.
+// used internal register: r0, r3
+//
 print:
-  // TODO: Fix this function too.
+  // prologue
+  stmdb r13!, {r14}
+
   ldrb r3,[r0]
 _loop:  
-  
+  bl putchar
 
   add r0, r0, #1
-  ldrb r3,[r0]
-  cmp r3,#0
+  ldrb r3, [r0]
+  cmp r3, #0
   bne _loop
-  mov r15, r14
+
+  // epilogue
+  ldmia r13!, {r14}
+  mov r15, r14 
 
 msg1: .asciz "First text.\n"
 msg2: .asciz "Second text!\n"
