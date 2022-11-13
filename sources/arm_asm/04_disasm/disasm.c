f2c997005586c35b25fce440a6bc71184f096e71
Atsushi Toyama <atsushi.tohyama.160.333@gmail.com>
2022-11-13 20:36:20 +0900

    Disassemble b instruction

--- sources/arm_asm/04_disasm/disasm.c
+++ sources/arm_asm/04_disasm/disasm.c
@@ -4,12 +4,29 @@ enum opcode {
     OP_MOV = 0b1101,
 };
 
+static int print_asm_mov(int word);
+static int print_asm_branch(int word);
+
 //
 // See the following ARM7DI data sheet for more detail:
 //
 // https://developer.arm.com/docs/ddi0027/latest/arm7di-data-sheet
 //
 static int print_asm(int word) {
+    int id = (word >> 25) & 0b111;
+    switch (id) {
+    case 0b001:
+        return print_asm_mov(word);
+        break;
+    case 0b101:
+        return print_asm_branch(word);
+        break;
+    default:
+        return -1;
+    }
+}
+
+static int print_asm_mov(int word) {
     int imm = word & 0xFF;
     int dst_reg = (word >> 12) & 0x0F;
     int opcode = (word >> 21) & 0x0F;
@@ -22,21 +39,30 @@ static int print_asm(int word) {
     return -1;
 }
 
+static int print_asm_branch(int word) {
+    int offset = word | 0xFF000000;
+
+    cl_printf("b [r15, #%d]\n", offset);
+    return 0;
+}
+
 #define ARR_SIZE(a) (int)(sizeof(a) / sizeof(a[0]))
 
 static void _test_disasm(void) {
     int words[] = {
+        // mov
         0xE3A01068,
         0xE3A01065,
         0xE3A0200D,
         0xE3A0200A,
+
+        // b
+        0xEAFFFFFE,
     };
 
     char *expecteds[] = {
-        "mov r1, #0x68\n",
-        "mov r1, #0x65\n",
-        "mov r2, #0xd\n",
-        "mov r2, #0xa\n",
+        "mov r1, #0x68\n", "mov r1, #0x65\n", "mov r2, #0xd\n",
+        "mov r2, #0xa\n",  "b [r15, #-2]\n",
     };
 
     cl_enable_buffer_mode();
