7d60e4dc599c07d6ba55bcdc56add35c83adeb36
Atsushi Toyama <atsushi.tohyama.160.333@gmail.com>
2022-11-13 09:20:20 +0900

    Refacter print_hex.s

--- sources/arm_asm/02_arm_assembly/2_3_print_hex/print_hex.s
+++ sources/arm_asm/02_arm_assembly/2_3_print_hex/print_hex.s
@@ -6,49 +6,57 @@
 */
 .globl _start
 _start:
-    ldr r1,=0xdeadbeaf
-    ldr r2,=0x101f1000
-    ldr r3,=hexchar
+    ldr r0,=0xdeadbeaf
 
-    // Calculate number of digits
-    mov r8, r1
-    mov r9, #0
-calc_num_of_digit:
-    add r9, r9, #1
-    lsr r8, r8, #4
-    cmp r8, #0
-    bne calc_num_of_digit
+//
+// print_hex:
+//   arg r0: target string which is printed.
+//   used internal register: r2, r4, r5.
+//
+print_hex:
+    // Set UART address.
+    ldr r2, =0x101f1000
 
-    // Print '0x'
+    // Print '0x'.
     mov r4, #0x30
     str r4, [r2]
     mov r4, #0x78
     str r4, [r2]
 
-print_hex:
-    // Get half byte. From most significant to least.
-    mov r4, r9
-    sub r4, #1
-    lsl r4, #2
-    lsr r4, r1, r4
-    and r4, r4, #0xF
+    // Set counter.
+    mov r4, #8
 
-    // Print hex
-    add r4, r4, r3
-    ldrb r5, [r4]
+_loop:
+    // Decrement counter.
+    sub r4, r4, #1
+
+    // Get half byte.
+    mov r5, r4
+    lsl r5, #2
+    lsr r5, r0, r5
+    and r5, r5, #0xF
+
+    // Determine alphabet ('A'-'F') or digit ('0'-'9').
+    cmp r5, #0x09
+    ble _digit
+
+    // Alphabet case.
+    add r5, r5, #0x37 // 'A' - 0x0a
+    b _print_char
+
+_digit:
+    add r5, r5, #0x30 // '0' - 0x00
+
+_print_char:
     str r5, [r2]
 
-    // Shift and jump if needed
-    sub r9, #1 // decrement counter
-    cmp r9, #0
-    bne print_hex
+    // Continue until 4 byte integer printed as hex.
+    cmp r4, #0
+    bne _loop
 
-    // Print newline
-    mov r4, #0x0A
-    str r4, [r2]
+    // Print newline.
+    mov r5, #0x0A
+    str r5, [r2]
 
 end:
     b end
-
-hexchar:
-    .asciz "0123456789ABCDEF"
