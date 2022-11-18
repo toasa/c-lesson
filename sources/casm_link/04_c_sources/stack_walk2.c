0b92831e7799d566750022f0f4dc07b7b7fa9573
Atsushi Toyama <atsushi.tohyama.160.333@gmail.com>
2022-11-18 17:09:32 +0900

    chore

--- sources/casm_link/04_c_sources/stack_walk2.c
+++ sources/casm_link/04_c_sources/stack_walk2.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 
-void print_address(int address) {
-    printf("address: %x\n", address);
-}
+void print_address(int address) { printf("  address: %x\n", address); }
 
-void print_msg(char *str) {
-    printf("We get (%s)\n", str);
-}
+void print_msg(char *str) { printf("  We get (%s)\n", str); }
 
-void func3 () {
+void func3() {
     // TODO: get main_msg from here as target someway.
     char *target;
 
@@ -27,11 +23,9 @@ void func1() {
     func2();
 }
 
-
 int main() {
-    char *main_msg = "We are in main.";
+    char *main_msg = "main main.";
     printf("We are in main, %s\n", main_msg);
     func1();
     return 0;
 }
-
