62cbd6a683361f1ae34a6665d34c6e2d28b10ffe
Atsushi Toyama <atsushi.tohyama.160.333@gmail.com>
2022-11-18 21:44:28 +0900

    Solve sum_range_inline.c

--- sources/casm_link/05_inline_asm/sum_range_inline.c
+++ sources/casm_link/05_inline_asm/sum_range_inline.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int sum_range(int beg, int end) {
-    int sum=0;
-    for(int i = beg; i <= end; i++) {
-        sum+=i;
+    int sum = 0;
+    for (int i = beg; i <= end; i++) {
+        sum += i;
     }
     return sum;
 }
@@ -11,37 +11,48 @@ int sum_range(int beg, int end) {
 int sum_range_inline(int beg, int end) {
     int res;
     /*
-    TODO: Fix code below to pass test.
-    */
-    asm("mov r2, #0");
-    asm("mov %0, r2" :"=r"(res));
+     * args
+     *   r0: beg
+     *   r1: end
+     *
+     * Use r2 as loop counter.
+     * Use r3 to store temp result.
+     */
+    asm("mov r2, r0");
+    asm("mov r3, #0");
+
+    asm("loop:");
+    asm("cmp r2, r1");
+    asm("bgt end");
+
+    asm("add r3, r3, r2"); // sum+=i
+    asm("add r2, r2, #1"); // i++
+    asm("b loop");
+
+    asm("end:");
+    asm("mov %0, r3" : "=r"(res));
 
     return res;
 }
-
 
 /*
 test code
 */
 
-
 void assert_true(int boolflag) {
-    if(!boolflag) {
+    if (!boolflag) {
         printf("assert fail\n");
     }
 }
 
-
 int main() {
     int res;
-    
+
     res = sum_range(3, 10);
     assert_true(res == 52);
 
-    // TODO: fix sum_range_inline to pass test.
     res = sum_range_inline(3, 10);
     assert_true(res == 52);
 
     return 0;
 }
-
