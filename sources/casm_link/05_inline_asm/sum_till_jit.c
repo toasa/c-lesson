1c88196d6f759cad6003f0e23c039198a9d0383c
Atsushi Toyama <atsushi.tohyama.160.333@gmail.com>
2022-11-18 22:27:55 +0900

    chore
    format

--- sources/casm_link/05_inline_asm/sum_till_jit.c
+++ sources/casm_link/05_inline_asm/sum_till_jit.c
@@ -2,9 +2,9 @@
 #include <sys/mman.h>
 
 int sum_till(int a) {
-    int sum=0;
-    for(int i = 0; i < a; i++) {
-        sum+=i;
+    int sum = 0;
+    for (int i = 0; i < a; i++) {
+        sum += i;
     }
     return sum;
 }
@@ -21,25 +21,22 @@ int sum_till_inline(int a) {
     asm("cmp r0, r1");
     asm("beq end");
     asm("add r2, r2, r1"); // sum+=i
-    asm("add r1, r1, #1"); // i++    
+    asm("add r1, r1, #1"); // i++
     asm("b loop");
     asm("end:");
-    asm("mov %0, r2" :"=r"(res));
+    asm("mov %0, r2" : "=r"(res));
     return res;
 }
-
 
 /*
 JIT
 */
 int *binary_buf;
 
-int* allocate_executable_buf(int size) {
-    return (int*)mmap(0, size,
-                 PROT_READ | PROT_WRITE | PROT_EXEC,
-                 MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+int *allocate_executable_buf(int size) {
+    return (int *)mmap(0, size, PROT_READ | PROT_WRITE | PROT_EXEC,
+                       MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 }
-
 
 void jit_sum_till() {
     /* TODO:
@@ -47,22 +44,19 @@ void jit_sum_till() {
     Hint: put almost the same binary as sum_till_inline.
     Compile and use arm-linux-gnueabi-objdump -S ./a.out to check binary.
     */
-    // dummy implementation 
+    // dummy implementation
     binary_buf[0] = 0xe1a0f00e; // mov r15, r14
 }
-
 
 /*
 test code
 */
 
-
 void assert_true(int boolflag) {
-    if(!boolflag) {
+    if (!boolflag) {
         printf("assert fail\n");
     }
 }
-
 
 int main() {
     int res;
@@ -75,12 +69,12 @@ int main() {
     assert_true(res == 45);
 
     /* TODO: Pass below code.
-    */
+     */
     binary_buf = allocate_executable_buf(1024);
 
     jit_sum_till();
 
-    funcvar = (int(*)(int))binary_buf;
+    funcvar = (int (*)(int))binary_buf;
     res = funcvar(10);
     assert_true(res == 45);
 
@@ -89,4 +83,3 @@ int main() {
 
     return 0;
 }
-
