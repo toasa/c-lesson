dda24f1ca95413b41897b3478fb293919b060f04
Atsushi Toyama <atsushi.tohyama.160.333@gmail.com>
2022-11-20 13:12:31 +0900

    Add emitter

--- sources/casm_link/06_jit_ps/ps_jit.c
+++ sources/casm_link/06_jit_ps/ps_jit.c
@@ -7,37 +7,54 @@
 #include "ps_jit.h"
 #include "ps_jit_test.h"
 
-static int *binary_buf = NULL;
+#define BUF_SIZE 1024
+static int *_binary_buf = NULL;
+
+struct Emitter {
+    int *binary_buf;
+    int pos;
+};
 
 static int *allocate_executable_buf(int size) {
     return (int *)mmap(0, size, PROT_READ | PROT_WRITE | PROT_EXEC,
                        MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 }
 
-static void ensure_jit_buf() {
-    if (binary_buf == NULL) {
-        binary_buf = allocate_executable_buf(1024);
-    }
+static struct Emitter *new_emitter(void) {
+    struct Emitter *e = calloc(1, sizeof(struct Emitter));
+
+    // We create buffer only once.
+    if (_binary_buf == NULL)
+        _binary_buf = allocate_executable_buf(BUF_SIZE);
+
+    e->binary_buf = _binary_buf;
+    e->pos = 0;
+
+    return e;
 }
 
-int *jit_script(char *input) {
-    ensure_jit_buf();
+static void emit(struct Emitter *e, int word) {
+    e->binary_buf[e->pos++] = word;
+}
 
+static int *emitter_get_buf(struct Emitter *e) { return e->binary_buf; }
+
+int *jit_script(char *input) {
+    struct Emitter *e = new_emitter();
     struct Substr remain = {input, strlen(input)};
-    int pos = 0;
 
     while (!is_end(&remain)) {
         skip_space(&remain);
         if (is_number(remain.ptr)) {
             int8_t imm = (int8_t)parse_number(remain.ptr);
-            binary_buf[pos++] = asm_mov_imm(R2, imm);
-            binary_buf[pos++] = asm_stfmd(R2);
+            emit(e, asm_mov_imm(R2, imm));
+            emit(e, asm_stfmd(R2));
 
             skip_token(&remain);
             continue;
         } else if (is_register(remain.ptr)) {
             int reg = remain.ptr[1] == '1' ? R1 : R0;
-            binary_buf[pos++] = asm_stfmd(reg);
+            emit(e, asm_stfmd(reg));
 
             skip_token(&remain);
             continue;
@@ -47,32 +64,32 @@ int *jit_script(char *input) {
             skip_token(&remain);
 
             // R2 is operand of left hand side and R3 is right.
-            binary_buf[pos++] = asm_ldmia(R3);
-            binary_buf[pos++] = asm_ldmia(R2);
+            emit(e, asm_ldmia(R3));
+            emit(e, asm_ldmia(R2));
             switch (op) {
             case OP_ADD:
-                binary_buf[pos++] = asm_add(R2, R2, R3);
+                emit(e, asm_add(R2, R2, R3));
                 break;
             case OP_SUB:
-                binary_buf[pos++] = asm_sub(R2, R2, R3);
+                emit(e, asm_sub(R2, R2, R3));
                 break;
             case OP_MUL:
-                binary_buf[pos++] = asm_mul(R2, R2, R3);
+                emit(e, asm_mul(R2, R2, R3));
                 break;
             }
 
-            binary_buf[pos++] = asm_stfmd(R2);
+            emit(e, asm_stfmd(R2));
         }
     }
 
     // Pop the calculation result and store it into r0 as return value.
-    binary_buf[pos++] = asm_ldmia(R2);
-    binary_buf[pos++] = asm_mov(R0, R2);
+    emit(e, asm_ldmia(R2));
+    emit(e, asm_mov(R0, R2));
 
     // Function epilogue.
-    binary_buf[pos++] = asm_mov(PC, LR);
+    emit(e, asm_mov(PC, LR));
 
-    return binary_buf;
+    return emitter_get_buf(e);
 }
 
 int main() {
